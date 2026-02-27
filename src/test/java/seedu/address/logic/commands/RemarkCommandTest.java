1917d41b792dc747ed6b3ae22576e84acdcdffe7
Guo Keming <e1398747@u.nus.edu>
2026-02-27 11:19:39 +0800

    Fix newline at EOF

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -120,4 +120,6 @@ public class RemarkCommandTest {
             assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON,
                     new Remark(VALID_REMARK_BOB))));
         }
+        
     }
+    
